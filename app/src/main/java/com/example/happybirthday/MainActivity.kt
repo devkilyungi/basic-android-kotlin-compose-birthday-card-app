caf142e2718e4bb64b794b782417baf2e1e79460
devkilyungi <victorkilyungi@gmail.com>
2023-04-19 11:48:40 +0300

    formats code

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -40,10 +40,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun BirthdayGreetingWithText (message: String, from: String, modifier: Modifier = Modifier){
-    Column(modifier = modifier
-        .padding(16.dp)
-        .fillMaxSize(),
+fun BirthdayGreetingWithImage (
+    message: String,
+    from: String,
+    modifier: Modifier = Modifier
+) {
+    val image = painterResource(id = R.drawable.androidparty)
+
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+        BirthdayGreetingWithText(
+            message = message,
+            from = from
+        )
+    }
+}
+
+@Composable
+fun BirthdayGreetingWithText (
+    message: String,
+    from: String,
+    modifier: Modifier = Modifier
+){
+    Column(
+        modifier = modifier.padding(16.dp).fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -54,23 +78,7 @@ fun BirthdayGreetingWithText (message: String, from: String, modifier: Modifier 
         Text(
             text = from,
             fontSize = 24.sp,
-            modifier = modifier.padding(top = 16.dp).align(Alignment.End),
-        )
-    }
-}
-
-@Composable
-fun BirthdayGreetingWithImage (message: String, from: String, modifier: Modifier = Modifier) {
-    val image = painterResource(id = R.drawable.androidparty)
-
-    Box {
-        Image(painter = image,
-            contentDescription = null,
-            contentScale = ContentScale.Crop
-        )
-        BirthdayGreetingWithText(
-            message = message,
-            from = from
+            modifier = modifier.padding(top = 8.dp).align(Alignment.End),
         )
     }
 }
